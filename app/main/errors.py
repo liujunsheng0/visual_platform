b37b12a269f2602a1c290b07949df8b10afa240e
liujunsheng <401865981@qq.com>
2018-11-12 15:11:36 +0800

    新增404,500页面

--- app/main/errors.py
+++ app/main/errors.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+from flask import render_template
+
 from . import main
 
 @main.app_errorhandler(404)
 def page_not_found(e):
-    return '404, %s' % str(e), 404
+    # return '404, %s' % str(e), 404
+    return render_template('error/404.html', error=str(e))
 
 
 @main.app_errorhandler(500)
 def internal_server_error(e):
-    return '500, %s' % str(e), 500
+    # return '500, %s' % str(e), 500
+    return render_template('error/500.html', error=str(e))
+
